d8d9201a3523e5b23441abc4e1ee3f597a2611d7
Renchie Yang <ryan0042@student.monash.edu>
2023-07-12 15:31:07 +1000

    completed billboards & categories tabs

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.jsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.jsx
@@ -14,6 +14,7 @@ import {
 import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { Trash } from "lucide-react";
@@ -30,6 +31,7 @@ const formSchema = z.object({
 export const SettingsForm = ({ initialData }) => {
     const params = useParams();
     const router = useRouter();
+    const origin = useOrigin();
 
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
